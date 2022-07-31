50567e3af677fd3703cefac6fbfca2fb57529969
Mark McLaughlin <mark@mclaughlin.me.uk>
2022-07-30 20:59:16 -0700

    Add pydoc comments

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+"""One-page Flask app to retrieve and render departures for a given station."""
 from flask import Flask, render_template
 from Huxley import Huxley
 
@@ -7,12 +8,14 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def hello_world():
+    """Return a default main page."""
     return "<p>Hello, World!</p>"
 
 
 @app.route("/departures/<string:crs>/")
 @app.route("/departures/<string:crs>/<int:rows>")
 def departures(crs=None, rows=10):
+    """Render a departure board for the given station."""
     station = Huxley(crs)
     station.get_departures(expand=False, rows=rows)
     return render_template("departures.jinja2", station=station)
