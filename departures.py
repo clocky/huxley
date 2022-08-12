40bc66d5a8c15f981e24e8d7b3c01da43511615e
Mark McLaughlin <mark@mclaughlin.me.uk>
2022-08-11 23:11:07 -0700

    feat: add click CLI support

--- departures.py
+++ departures.py
@@ -1,10 +1,13 @@
-from Huxley import Huxley
-from rich import print
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import click
 from rich.console import Console
 from rich.table import Table
 from rich.theme import Theme
 
-bootstrap = Theme(
+from Huxley import Huxley
+
+BOOTSTRAP = Theme(
     {
         "success": "#198754",
         "warning": "#ffc107",
@@ -18,13 +21,15 @@ bootstrap = Theme(
 
 
 def show_departures(station):
-    console = Console(theme=bootstrap)
+    console = Console(theme=BOOTSTRAP)
+
     table = Table(style="dark")
     table.add_column("Time", width=6)
     table.add_column("Destination", style="warning", width=40)
     table.add_column("Plat", justify="right", width=4)
     table.add_column("Expected", justify="right", width=9)
-    table.add_column("Operator", justify="right", style="info", width=24)
+    table.add_column("Operator", justify="right", style="info")
+
     for service in station.train_services:
         std: str = service.std
         etd: str = service.etd
@@ -32,11 +37,12 @@ def show_departures(station):
         operator: str = service.operator
         destination: str = ""
         via: str = ""
+
         for d in service.destination:
             destination = d.location_name
             if hasattr(d, "via"):
                 if d.via is not None:
-                    destination = f"{destination} [secondary]{d.via}[/secondary]"
+                    destination = f"{destination} [white]{d.via}[/white]"
 
         if service.etd == "On time":
             etd = f"[success]{etd}[/success]"
@@ -46,10 +52,25 @@ def show_departures(station):
             etd = f"[warning]{etd}[/warning]"
 
         table.add_row(std, destination, platform, etd, operator)
+
+        if hasattr(service, "cancel_reason"):
+            if service.cancel_reason is not None and service.is_cancelled is True:
+                table.add_row("", f"[secondary]{service.cancel_reason}[/secondary]")
+
+        if hasattr(service, "delay_reason"):
+            if service.delay_reason is not None and service.cancel_reason is None:
+                table.add_row("", f"[secondary]{service.delay_reason}[/secondary]")
     console.print(table)
 
 
-if __name__ == "__main__":
-    station = Huxley("wok")
-    station.get_departures(expand=False, rows=10)
+@click.command()
+@click.option("--crs", prompt="CRS Code", help="CRS Code")
+@click.option("--rows", prompt="Number of rows", help="Number of rows")
+def departures(crs, rows):
+    station = Huxley(crs)
+    station.get_departures(expand=False, rows=rows)
     show_departures(station)
+
+
+if __name__ == "__main__":
+    departures()
